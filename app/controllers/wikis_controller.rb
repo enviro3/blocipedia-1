05799cd7b1775002558c8a1138eff7988a840e00
Sandy Tadeo <stadeo17@gmail.com>
2016-06-21 15:50:57 +0000

    Completed Checkpoint 4: Wiki CRUD

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,14 +1,74 @@
 class WikisController < ApplicationController
+
   def index
     @wikis = Wiki.all
+    #authorize @wikis
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
+    #authorize @wiki
   end
 
   def new
+    #@user = my_user
+    @wiki = Wiki.new
+    #authorize @wiki
+  end
+
+  def create
+    @wiki = Wiki.new(user: current_user)
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    
+    if @wiki.save
+      flash[:notice] = "Post was saved successfully."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+     
+    # @wiki = current_user.wikis.create(wiki_params)
+    # authorize @wiki
+    # # adding owner to collaborator list
+    # # collaborator = @wiki.collaborators.build(user_id: current_user.id)
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  #   @wiki = Wiki.find(params[:id])
+  #   authorize @wiki
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+  #   @wiki = Wiki.find(params[:id])
+  #   authorize @wiki
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated successfully."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+  
+  def destroy
+    @wiki = Wiki.find(params[:id])
+  #   title = @wiki.title
+  #   authorize @wiki
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create Users
+5.times do
+  User.create!(
+    #name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+
+# Create admin user
+unless User.find_by(email: 'admin@example.com')
+  User.create!(
+    #name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'helloworld',
+    #role: 'admin'
+  )
+end
+
+unless User.find_by(email: 'member@example.com')
+  User.create!(
+    #name: 'Member User',
+    email: 'member@example.com',
+    password: 'helloworld'
+  )
+end
+users = User.all
+
+puts "#{User.count} users created"
+
+# Create Wikis
+10.times do
+  Wiki.create!(
+    user: users.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+
+wikis = Wiki.all
+
+puts "#{Wiki.count} posts created"
+
+puts "Seed finished"
