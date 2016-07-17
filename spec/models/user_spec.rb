7d2d54602acd5f8df5c5119612b82de88c9db50a
Xander Miller <xander.miller@gmail.com>
2016-07-17 14:41:09 +0000

    cleans up model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  let(:user) { create(:user) }
-
-  before :example do
-    sign_in my_user
+  describe "factory" do
+    it "has a valid factory" do
+      expect(build(:user)).to be_valid
+    end
   end
-    
+
   describe "attributes" do
     it { should have_db_column(:name).of_type(:string) }
     it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:role).of_type(:integer).with_options(default: 0, null: false) }
     it { should define_enum_for(:role).with([:standard, :premium, :admin]) }
+  end
+  
+  describe "associations" do
+  end
+  
+  describe "validations" do
   end
     
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  
-  let(:post) { create(:wiki) }
-  let(:user) { create(:user) }
-  
+
   describe "attributes" do
     it { should have_db_column(:title).of_type(:string) }
     it { should have_db_column(:body).of_type(:text) }
