8159fadf2f2599c670690bed6d2d6b01eb795e20
Xander Miller <xander.miller@gmail.com>
2016-07-17 15:08:00 +0000

    adds conditional to user email downcasing callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
+  # Associations
+  
   has_many :wikis, dependent: :destroy
   #has_many :collaborators
   #has_many :wikis, through: :collaborators
   
   enum role: [:standard, :premium, :admin]
   
-
-  before_save { self.email = email.downcase }
-  after_initialize { self.role ||= :standard }
+  # Validations
   
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  # Call back
+  before_save { self.email = email.downcase if self.email.present? }
 
   
   #def downgrade
